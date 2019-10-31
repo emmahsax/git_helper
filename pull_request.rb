e176fc3a48997e766588393f0f1c98d8190e3959
Emma Sax <emma.sax4+git@gmail.com>
2019-10-31 18:59:50 -0500

    Slowly making improvements

--- pull_request.rb
+++ pull_request.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require_relative './octokit_client.rb'
 
 class PullRequest
@@ -11,34 +13,43 @@ class PullRequest
     end
   end
 
-  def create_pull_request
+  def create
     begin
       pr = octokit_client.create_pull_request(repository_from_local, "master", branch_from_local, @pr_title)
       puts "Pull request successfully created: #{pr.html_url}"
     rescue Octokit::UnprocessableEntity => e
-      puts "Could not create pull request:"
-      if e.message.include?("pull request already exists")
-        puts "  A pull request already exists for this branch"
-      elsif e.message.include?("No commits between master and")
-        puts "  No commits have been pushed to GitHub"
-      elsif e.message.include?("422 - Invalid request")
-        puts "  No pull request title was provided"
+      puts 'Could not create pull request:'
+      if e.message.include?('pull request already exists')
+        puts '  A pull request already exists for this branch'
+      elsif e.message.include?('No commits between master and')
+        puts '  No commits have been pushed to GitHub'
       else
         puts e.message
       end
     end
   end
 
-  def merge_pull_request
+  def merge
     begin
       merge = octokit_client.merge_pull_request(repository_from_local, @pr_number, commit_message)
       puts "Pull request successfully merged: #{merge.sha}"
     rescue Octokit::UnprocessableEntity => e
-      puts "Could not merge pull request:"
+      puts 'Could not merge pull request:'
       puts e.message
     rescue Octokit::NotFound => e
-      puts "Could not merge pull request:"
-      puts "  Could not a locate a pull request to merge"
+      puts 'Could not merge pull request:'
+      puts '  Could not a locate a pull request to merge'
+    rescue Octokit::MethodNotAllowed => e
+      puts 'Could not merge pull_request:'
+      if e.message.include?('405 - Required status check')
+        puts '  A required status check has not passed'
+      elsif e.message.include?('405 - Base branch was modified')
+        puts '  The base branch has been modified'
+      elsif e.message.include?('405 - Pull Request is not mergeable')
+        puts '  The pull request is not mergeable'
+      else
+        puts e.message
+      end
     end
   end
 
@@ -75,11 +86,38 @@ class PullRequest
   end
 end
 
-pr = PullRequest.new(ARGV[1])
-action = ARGV[0].to_sym
+arg = ARGV[0]
+option = ARGV[1]
 
-if action == :create
-  pr.create_pull_request
-elsif action == :merge
-  pr.merge_pull_request
+case arg
+when '-c', '--create'
+  action = :create
+when '-m', '--merge'
+  action = :merge
+when '-h', '--help', nil, ''
+  puts """
+Usage for working with this pull requests script:
+  # Run this script from within your local repository/branch
+  ruby pull_request.rb [-h|-c|-m] {option}
+
+  -h, --help      - Displays this help information
+  -c, --create    - Create a new pull request with a title (the next argument)
+  -m, --merge     - Merge the pull request that corresponds to the PR number (the next argument)
+
+Required: create or merge, option
+Examples:
+  ruby pull_request.rb -c 'Title of pull request'
+  ruby pull_request.rb -m 101
+    """
+    exit(0)
+end
+
+puts "\nAttempting to #{action.to_s} pull request: #{option}", ''
+pull_request = PullRequest.new(option)
+
+case action
+when :create
+  pull_request.create
+when :merge
+  pull_request.merge
 end
