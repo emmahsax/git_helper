1f1889f684dec5e66fbe38db2f1ac968ae31827b
Emma Sax <emma.sax4@gmail.com>
2019-10-31 19:20:33 -0500

    Accept an optional user to merge/create pull requests with

--- octokit_client.rb
+++ octokit_client.rb
@@ -4,16 +4,22 @@ require 'octokit'
 class OctokitClient
   GITHUB_CONFIG_FILE = ".automation/config.yml"
 
+  attr_accessor :user
+
+  def initialize(user=nil)
+    @user = user
+  end
+
   def client
     Octokit::Client.new(access_token: github_token)
   end
 
   def github_username
-    config_file[:github_user]
+    @user ? config_file[@user][:github_user] : config_file[:github_user]
   end
 
   private def github_token
-    config_file[:github_token]
+    @user ? config_file[@user][:github_token] : config_file[:github_token]
   end
 
   private def config_file
