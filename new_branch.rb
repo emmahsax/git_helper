f56841ae7009be60adc618c4293d33a3e1557fb1
Emma Sax <emma.sax4+git@gmail.com>
2019-11-14 10:46:47 -0600

    Ask users questions about what they're trying to do

--- new_branch.rb
+++ new_branch.rb
@@ -1,37 +1,21 @@
 #!/usr/bin/env ruby
 
+require_relative './highline_cli.rb'
+
 class NewBranch
-  attr_accessor :branch_name
-
-  def initialize(branch_name)
-    @branch_name = branch_name
-  end
-
   def create_new_branch
-    system("git branch --no-track #{@branch_name}")
-    system("git checkout #{@branch_name}")
-    system("git branch --set-upstream-to=origin #{@branch_name}")
+    branch_name = cli.ask('New branch name?')
+    puts "Attempting to create a new branch: #{branch_name}"
+    system("git branch --no-track #{branch_name}")
+    system("git checkout #{branch_name}")
+    system("git branch --set-upstream-to=origin #{branch_name}")
     system("git pull")
     system("git push")
   end
+
+  private def cli
+    @cli ||= HighlineCli.new
+  end
 end
 
-arg = ARGV[0]
-
-if arg == '-h' || arg == '--help' || arg.nil? || arg == ''
-  puts """
-Usage for creating new branches:
-  # Run this script from within your local repository/branch
-  ./new_branch.rb {branch_name}
-
-  -h, --help      - Displays this help information
-
-Required: branch_name
-Examples:
-  ./new_branch.rb my_new_feature_branch
-    """
-    exit(0)
-end
-
-puts "\nAttempting to create a new branch: #{arg}", ''
-NewBranch.new(arg).create_new_branch
+NewBranch.new.create_new_branch
