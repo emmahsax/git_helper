214f2f1e9d0f99223b2af4431df2a54b7cfb1ad1
Emma Sax <emma.sax4@gmail.com>
2020-07-31 14:51:02 -0500

    Oops! I think this wrong flag may be causing my issues!

--- new_branch.rb
+++ new_branch.rb
@@ -8,7 +8,7 @@ class NewBranch
     puts "Attempting to create a new branch: #{branch_name}"
     system("git branch --no-track #{branch_name}")
     system("git checkout #{branch_name}")
-    system("git branch --set-upstream-to=origin #{branch_name}")
+    system("git branch --set-upstream origin #{branch_name}")
     system("git pull")
     system("git push")
   end
