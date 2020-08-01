941915201c786876bdbe39e28828f21b6fdde40b
Emma Sax <emma.sax4+git@gmail.com>
2020-08-01 10:25:35 -0500

    Try this

--- new_branch.rb
+++ new_branch.rb
@@ -8,9 +8,9 @@ class NewBranch
     puts "Attempting to create a new branch: #{branch_name}"
     system("git branch --no-track #{branch_name}")
     system("git checkout #{branch_name}")
-    system("git branch --set-upstream origin #{branch_name}")
-    system("git pull")
-    system("git push")
+    system("git push --set-upstream origin #{branch_name}")
+    # system("git pull")
+    # system("git push")
   end
 
   private def cli
