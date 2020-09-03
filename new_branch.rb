76e8a4db9dd6de4d2a2b43a77c36c0a40e4c95f1
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-09-03 09:13:49 -0500

    Git pull before doing anything else

--- new_branch.rb
+++ new_branch.rb
@@ -6,6 +6,7 @@ class NewBranch
   def create_new_branch
     branch_name = cli.ask('New branch name?')
     puts "Attempting to create a new branch: #{branch_name}"
+    system("git pull")
     system("git branch --no-track #{branch_name}")
     system("git checkout #{branch_name}")
     system("git push --set-upstream origin #{branch_name}")
