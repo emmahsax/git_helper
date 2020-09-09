a40f05bfe9bba3116e864ff44bf6f75fd0f42b3a
Emma Sax <emma.sax4+git@gmail.com>
2020-09-09 11:12:22 -0500

    Switch a bunch of scripts to be an official Ruby gem! (#16)
    * Turn this repo into a gem
    
    * Move these around
    
    * Create it into a gem
    
    * Woohoo!
    
    * Break accordingly
    
    * We don't really need this
    
    * Make change_remote work with https and ssh
    
    * Update README
    
    * Here we go
    
    * Specify these are all bash

---
+++ lib/git_helper/new_branch.rb
@@ -0,0 +1,18 @@
+require_relative './highline_cli.rb'
+
+module GitHelper
+  class NewBranch
+    def execute(new_branch_name = nil)
+      branch_name = new_branch_name || cli.ask('New branch name?')
+      puts "Attempting to create a new branch: #{branch_name}"
+      system("git pull")
+      system("git branch --no-track #{branch_name}")
+      system("git checkout #{branch_name}")
+      system("git push --set-upstream origin #{branch_name}")
+    end
+
+    private def cli
+      @cli ||= GitHelper::HighlineCli.new
+    end
+  end
+end
---
+++ lib/git_helper/octokit_client.rb
@@ -0,0 +1,14 @@
+require 'octokit'
+require_relative './git_config_reader.rb'
+
+module GitHelper
+  class OctokitClient
+    def client
+      Octokit::Client.new(access_token: git_config_reader.github_token)
+    end
+
+    private def git_config_reader
+      @git_config_reader ||= GitHelper::GitConfigReader.new
+    end
+  end
+end
