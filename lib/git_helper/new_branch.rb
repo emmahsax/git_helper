2ddcaf4d6df29d58bdbb301ad80798904739e754
Emma Sax <sax.emma.h@gmail.com>
2020-10-01 18:05:14 -0500

    Use shareable classes (#28)
    Use shareable classes

--- lib/git_helper/new_branch.rb
+++ lib/git_helper/new_branch.rb
@@ -4,17 +4,9 @@ require_relative './local_code.rb'
 module GitHelper
   class NewBranch
     def execute(new_branch_name = nil)
-      branch_name = new_branch_name || cli.new_branch_name
+      branch_name = new_branch_name || GitHelper::HighlineCli.new.new_branch_name
       puts "Attempting to create a new branch: #{branch_name}"
-      local_code.new_branch(branch_name)
-    end
-
-    private def cli
-      @cli ||= GitHelper::HighlineCli.new
-    end
-
-    private def local_code
-      @local_code ||= GitHelper::LocalCode.new
+      GitHelper::LocalCode.new.new_branch(branch_name)
     end
   end
 end
