a0d41121229bd0484672cdbbf15bacba47aa687c
Emma Sax <7562793+emmasax4@users.noreply.github.com>
2020-10-01 12:28:58 -0500

    By default, create MRs with squashing set to true, and remove_source_branch set to true

--- lib/git_helper/merge_request.rb
+++ lib/git_helper/merge_request.rb
@@ -12,7 +12,7 @@ module GitHelper
         options = {
           source_branch: local_branch,
           target_branch: base_branch,
-          squash_merge_request: true,
+          squash: true,
           remove_source_branch: true,
           description: new_mr_body
         }
