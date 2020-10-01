3f75f64c4141800ce8d58b1026008a732864586a
Emma Sax <emma.the.coder@mail.com>
2020-10-01 09:33:41 -0500

    Refactoring (#24)
    * Refactoring!
    
    * Refactor the merge request script
    
    * More refactoring
    
    * Don't use the return word a lot
    
    * Make the new branch file also use the improved, shared classes
    
    * Remove comments
    
    * Fixes based on QA
    
    * Additional fixes from QA
    
    * Might as well go up by one more minor version
    
    Non-breaking changes, but updates to allow more flexibility when auto-generating titles of pull/merge requests

--- lib/git_helper/new_branch.rb
+++ lib/git_helper/new_branch.rb
@@ -1,18 +1,20 @@
 require_relative './highline_cli.rb'
+require_relative './local_code.rb'
 
 module GitHelper
   class NewBranch
     def execute(new_branch_name = nil)
-      branch_name = new_branch_name || cli.ask('New branch name?')
+      branch_name = new_branch_name || cli.new_branch_name
       puts "Attempting to create a new branch: #{branch_name}"
-      system("git pull")
-      system("git branch --no-track #{branch_name}")
-      system("git checkout #{branch_name}")
-      system("git push --set-upstream origin #{branch_name}")
+      local_code.new_branch(branch_name)
     end
 
     private def cli
       @cli ||= GitHelper::HighlineCli.new
+    end
+
+    private def local_code
+      @local_code ||= GitHelper::LocalCode.new
     end
   end
 end
