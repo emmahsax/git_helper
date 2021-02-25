a152d68bee25f7f163db19daf1dba3fd82128842
Emma Sax <emma.sax4+git@gmail.com>
2021-02-24 19:51:03 -0600

    Authenticate with GitHub API (#48)
    * Authenticate with the API
    
    * Reorder these methods

--- lib/git_helper/setup.rb
+++ lib/git_helper/setup.rb
@@ -10,7 +10,10 @@ module GitHelper
 
       create_or_update_config_file if answer
 
-      answer = highline.ask_yes_no("Do you wish to set up the Git Helper plugins? (y/n)")
+      answer = highline.ask_yes_no(
+        "Do you wish to set up the Git Helper plugins? (y/n) (This process will " \
+        "attempt to use your GitHub personal access token to authenticate)"
+      )
 
       if answer
         create_or_update_plugin_files
@@ -61,27 +64,33 @@ module GitHelper
       end
     end
 
-    private def highline
-      @highline ||= GitHelper::HighlineCli.new
-    end
-
-    private def config_file
-      GitHelper::GitConfigReader.new.git_config_file_path
-    end
-
     private def create_or_update_plugin_files
       plugins_dir = "#{Dir.pwd.scan(/\A\/[\w]*\/[\w]*\//).first}/.git_helper/plugins"
       plugins_url = 'https://api.github.com/repos/emmahsax/git_helper/contents/plugins'
       header = 'Accept: application/vnd.github.v3.raw'
+      token = git_config_reader.github_token
+      user = git_config_reader.github_user
 
       Dir.mkdir(plugins_dir) unless File.exists?(plugins_dir)
 
-      all_plugins = JSON.parse(`curl -s -H "#{header}" -L "#{plugins_url}"`)
+      all_plugins = JSON.parse(`curl -s -u #{user}:#{token} -H "#{header}" -L "#{plugins_url}"`)
 
       all_plugins.each do |plugin|
-        plugin_content = `curl -s -H "#{header}" -L "#{plugins_url}/#{plugin['name']}"`
+        plugin_content = `curl -s -u #{user}:#{token} -H "#{header}" -L "#{plugins_url}/#{plugin['name']}"`
         File.open("#{plugins_dir}/#{plugin['name']}", 'w') { |file| file.puts plugin_content }
       end
+    end
+
+    private def config_file
+      git_config_reader.git_config_file_path
+    end
+
+    private def git_config_reader
+      @git_config_reader ||= GitHelper::GitConfigReader.new
+    end
+
+    private def highline
+      @highline ||= GitHelper::HighlineCli.new
     end
   end
 end
