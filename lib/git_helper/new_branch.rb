3001ead3464cb20a1f60fe124bd51c203603b05f
Emma Sax <emma.sax4+git@gmail.com>
2021-03-03 17:14:42 -0600

    Two newlines

--- lib/git_helper/new_branch.rb
+++ lib/git_helper/new_branch.rb
@@ -4,7 +4,7 @@ module GitHelper
   class NewBranch
     def execute(new_branch_name = nil)
       branch_name = new_branch_name || HighlineWrapper.new.ask('New branch name?', { required: true })
-      puts "Attempting to create a new branch: #{branch_name}\n"
+      puts "Attempting to create a new branch: #{branch_name}\n\n"
       GitHelper::LocalCode.new.new_branch(branch_name)
     end
   end
