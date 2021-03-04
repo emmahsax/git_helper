97e0c368e4354aa61a05e2689ae48e4506e5acda
Emma Sax <sax.emma.h@gmail.com>
2021-03-03 21:19:06 -0600

    New version of Highline Wrapper (#52)
    * Use new version of highline_wrapper with question options
    
    * Cleaning up the newlines
    
    * Here we go
    
    * Here we go
    
    * Two newlines
    
    * Pull from the official highline_wrapper gem

--- lib/git_helper/new_branch.rb
+++ lib/git_helper/new_branch.rb
@@ -3,8 +3,8 @@
 module GitHelper
   class NewBranch
     def execute(new_branch_name = nil)
-      branch_name = new_branch_name || HighlineWrapper.new.ask('New branch name?')
-      puts "Attempting to create a new branch: #{branch_name}"
+      branch_name = new_branch_name || HighlineWrapper.new.ask('New branch name?', { required: true })
+      puts "Attempting to create a new branch: #{branch_name}\n\n"
       GitHelper::LocalCode.new.new_branch(branch_name)
     end
   end
